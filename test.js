acaa3d59a6ab06edf3d6115039d5af241ba4a96e
Pratham <pranka0789@gmail.com>
2026-02-02 13:34:51 +0530

    updated test

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
-// test.js
 import { explainError } from "./src/index.js";
-
-console.log("test.js running");
 
 try {
   throw new Error("random failure");
 } catch (e) {
   const result = explainError(e);
-  console.log("TYPE:", typeof result);
   console.log(result);
 }
